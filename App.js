c36fa7c7961b8ac78f61f73ab6ea0d4ea93399ec
Max Huang <letmaxknow@gmail.com>
2020-03-24 21:54:39 -0400

    Added native base

--- App.js
+++ App.js
@@ -23,6 +23,15 @@ class App extends React.Component {
     text: 'Pear',
   }];
 
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+  }
+
   render() {
 
     return (
