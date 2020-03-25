357fb1868939489b076d644850b1f16d36ff9512
Max Huang <letmaxknow@gmail.com>
2020-03-25 14:05:08 -0400

    Revert "Added native base"
    This reverts commit c36fa7c7961b8ac78f61f73ab6ea0d4ea93399ec.

--- App.js
+++ App.js
@@ -23,15 +23,6 @@ class App extends React.Component {
     text: 'Pear',
   }];
 
-  async componentDidMount() {
-    await Font.loadAsync({
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-      ...Ionicons.font,
-    });
-    this.setState({ isReady: true });
-  }
-
   render() {
 
     return (
